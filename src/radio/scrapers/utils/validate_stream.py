da0accaa40b9cdd2a5fa7c55f14277e8179887b7
Ashish Kumar <ashish.kmr@outlook.com>
2026-02-20 11:55:16 +0530

    move files

--- src/radio/scrapers/utils/validate_stream.py
+++ src/radio/scrapers/utils/validate_stream.py
@@ -6,7 +6,7 @@ import sys
 from datetime import datetime
 
 # Configuration
-DEFAULT_TIMEOUT = 15
+DEFAULT_TIMEOUT = 25
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 REFERER = "https://onlineradiobox.com/"
 
